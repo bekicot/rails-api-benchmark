4e814fe4c0f4cc9cb8cf590f5fe2068828f89035
Yana Agun Siswanto <yana.developer@gmail.com>
2017-02-25 06:27:23 +0700

    Install devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+yana = User.where(email: 'yana.developer@gmail.com').first_or_initialize
+yana.assign_attributes(
+  password: 123123123,
+  password_confirmation: 123123123
+)
+yana.save!
